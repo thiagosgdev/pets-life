33742482a6cd8a0c7b039ecf8157eaa072fdd4c9
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-11-07 21:33:05 -0300

    feat: ensure DbAddPet returns null if LoadPetByChipNumber finds a  pet

--- src/data/useCases/pet/db-add-pet.ts
+++ src/data/useCases/pet/db-add-pet.ts
@@ -1,16 +1,21 @@
 import { AddPetsRepository } from "@/data/protocols/db/pet/add-pets-repository";
 import { LoadPetByChipNumber } from "@/data/protocols/db/pet/load-pet-by-chip-number";
 import { PetModel } from "@/domain/models/pet";
-import { AddPetParams } from "@/domain/useCases/pet/add-pet";
+import { AddPet, AddPetParams } from "@/domain/useCases/pet/add-pet";
 
-export class DbAddPet implements AddPetsRepository {
+export class DbAddPet implements AddPet {
     constructor(
         private readonly addPetsRepository: AddPetsRepository,
         private readonly loadPetByChipNumber: LoadPetByChipNumber,
     ) {}
     async add(petInfo: AddPetParams): Promise<PetModel> {
-        await this.loadPetByChipNumber.loadByChipNumber(petInfo.chip_number);
-        const newPet = await this.addPetsRepository.add(petInfo);
-        return newPet;
+        const pet = await this.loadPetByChipNumber.loadByChipNumber(
+            petInfo.chip_number,
+        );
+        if (!pet) {
+            const newPet = await this.addPetsRepository.add(petInfo);
+            return newPet;
+        }
+        return null;
     }
 }
