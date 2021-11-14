e3d0a0fb9ac62a55706b8751d00560f174c13d52
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-11-14 10:09:14 -0300

    feat: ensure PetPostgresRepository return a pet on loadByChipNumber success

--- src/infra/db/typeorm/pet/pet-postgres-repository.ts
+++ src/infra/db/typeorm/pet/pet-postgres-repository.ts
@@ -1,10 +1,13 @@
+import { LoadPetByChipNumberRepository } from "@/data/protocols/db/pet/load-pet-by-chip-number-repository";
 import { Pet } from "@/domain/entities/Pet";
 import { PetModel } from "@/domain/models/pet";
 import { AddPet, AddPetParams } from "@/domain/useCases/pet/add-pet";
 import { LoadPetsByAccountId } from "@/domain/useCases/pet/load-pets-by-account-id";
 import { getRepository, Repository } from "typeorm";
 
-export class PetPostgresRepository implements AddPet, LoadPetsByAccountId {
+export class PetPostgresRepository
+    implements AddPet, LoadPetsByAccountId, LoadPetByChipNumberRepository
+{
     private repository: Repository<Pet>;
 
     constructor() {
@@ -22,5 +25,10 @@ export class PetPostgresRepository implements AddPet, LoadPetsByAccountId {
             return pets;
         }
         return null;
+    }
+
+    async loadByChipNumber(chip_number: string): Promise<PetModel> {
+        const pet = await this.repository.findOne({ chip_number });
+        return pet;
     }
 }
