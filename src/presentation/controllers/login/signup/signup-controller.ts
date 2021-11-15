b42f0d6b3534647118f35450a9f1f120b3102bc1
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-11-14 21:03:58 -0300

    feat: ensure SignupController returns status 500 if AddAccount throws

--- src/presentation/controllers/login/signup/signup-controller.ts
+++ src/presentation/controllers/login/signup/signup-controller.ts
@@ -5,11 +5,18 @@ import { HttpRequest, HttpResponse } from "@/presentation/protocols/http";
 export class SignUpController implements Controller {
     constructor(private readonly addAccount: AddAccount) {}
     async handle(request: HttpRequest): Promise<HttpResponse> {
-        const account = await this.addAccount.add(request.body);
-        if (account) {
+        try {
+            const account = await this.addAccount.add(request.body);
+            if (account) {
+                return {
+                    status: 200,
+                    body: account,
+                };
+            }
+        } catch (error) {
             return {
-                status: 200,
-                body: account,
+                status: 500,
+                body: error,
             };
         }
     }
