def13ac37fd70fb1d2d27d5c24ad103cc336bacd
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-11-16 06:35:50 -0300

    test: ensure RequiredFieldValidation returns MissingParamError if validation fails

---
+++ src/validation/require-field-validation.spec.ts
@@ -0,0 +1,14 @@
+import { MissingParamError } from "@/presentation/errors/missing-param-error";
+import { RequiredFieldValidation } from "./require-field-validation";
+
+const makeSut = (): RequiredFieldValidation => {
+    return new RequiredFieldValidation("field");
+};
+
+describe("Required Field Validation", () => {
+    test("Should return a MissingParamError if validation fails", () => {
+        const sut = makeSut();
+        const error = sut.validate({ name: "any_name" });
+        expect(error).toEqual(new MissingParamError("field"));
+    });
+});
