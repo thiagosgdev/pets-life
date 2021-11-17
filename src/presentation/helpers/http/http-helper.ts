4f57231b8b147c9f9273629ea3452ee904be653a
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-11-17 06:46:40 -0300

    feat: added new HttpHelper protocols for HttpResponses

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,8 +1,57 @@
 import { HttpResponse } from "@/presentation/protocols/http";
 
+export const serverError = (error: Error): HttpResponse => {
+    return {
+        status: 500,
+        body: error,
+    };
+};
+
 export const badRequest = (error: Error): HttpResponse => {
     return {
         status: 400,
         body: error,
+    };
+};
+
+export const unauthorized = (error: Error): HttpResponse => {
+    return {
+        status: 401,
+        body: error,
+    };
+};
+
+export const forbidden = (error: Error): HttpResponse => {
+    return {
+        status: 403,
+        body: error,
+    };
+};
+
+export const notFound = (error: Error): HttpResponse => {
+    return {
+        status: 404,
+        body: error,
+    };
+};
+
+export const ok = (message: any): HttpResponse => {
+    return {
+        status: 200,
+        body: message,
+    };
+};
+
+export const created = (message: any): HttpResponse => {
+    return {
+        status: 201,
+        body: message,
+    };
+};
+
+export const noContent = (message: any): HttpResponse => {
+    return {
+        status: 204,
+        body: message,
     };
 };
