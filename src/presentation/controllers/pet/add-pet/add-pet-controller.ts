46f94e3b9f29a9ba83061ef2ea315e78d42a5628
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-02 18:43:22 -0300

    feat: ensure AddPetController returns 500 if AddPet throws

--- src/presentation/controllers/pet/add-pet/add-pet-controller.ts
+++ src/presentation/controllers/pet/add-pet/add-pet-controller.ts
@@ -9,30 +9,37 @@ export class AddPetController implements Controller {
     constructor(private readonly addPet: AddPet) {}
 
     async handle(request: HttpRequest): Promise<HttpResponse> {
-        const {
-            name,
-            birthdate,
-            gender,
-            chip_number,
-            chip_website,
-            breed,
-            weigth,
-            account_id,
-        } = request.body;
-        const pet = await this.addPet.add({
-            name,
-            birthdate,
-            gender,
-            chip_number,
-            chip_website,
-            breed,
-            weigth,
-            account_id,
-        });
+        try {
+            const {
+                name,
+                birthdate,
+                gender,
+                chip_number,
+                chip_website,
+                breed,
+                weigth,
+                account_id,
+            } = request.body;
+            const pet = await this.addPet.add({
+                name,
+                birthdate,
+                gender,
+                chip_number,
+                chip_website,
+                breed,
+                weigth,
+                account_id,
+            });
 
-        return {
-            status: 200,
-            body: pet,
-        };
+            return {
+                status: 200,
+                body: pet,
+            };
+        } catch (error) {
+            return {
+                status: 500,
+                body: "Internal server error",
+            };
+        }
     }
 }
