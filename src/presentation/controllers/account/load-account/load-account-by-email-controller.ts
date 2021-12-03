4080537044c3ba9858a184ac663c2d4dc5d12d09
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-03 08:23:39 -0300

    feat: ensure LoadAccountByEmailController returns 200 on LoadAccountByEmail success

--- src/presentation/controllers/account/load-account/load-account-by-email-controller.ts
+++ src/presentation/controllers/account/load-account/load-account-by-email-controller.ts
@@ -9,7 +9,13 @@ export class LoadAccountByEmaiLController implements Controller {
     constructor(private readonly loadAccountByEmail: LoadAccountByEmail) {}
     async handle(request: HttpRequest): Promise<HttpResponse> {
         const { email } = request.body;
-        await this.loadAccountByEmail.load(email);
+        const account = await this.loadAccountByEmail.load(email);
+        if (account) {
+            return {
+                status: 200,
+                body: account,
+            };
+        }
         return null;
     }
 }
