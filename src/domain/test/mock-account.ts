e57d4d932d5f34cb29ef1d59a6b43aa28a06d79f
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-03 08:20:13 -0300

    test: ensure LoadAccountByEmailController calls LoadAccountByEmail with the correct values

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,5 +1,6 @@
 import { AddAccountParams } from "@/domain/useCases/account/add-account";
 import { AccountModel } from "@/domain/models/account";
+import { LoadAccountByEmail } from "../useCases/account/load-account-by-email";
 
 export const mockAddAccountParams = (): AddAccountParams => ({
     name: "any_name",
@@ -21,3 +22,12 @@ export const mockAccountModel = (): AccountModel => ({
     created_at: new Date(),
     updated_at: new Date(),
 });
+
+export const mockLoadAccountByEmail = (): LoadAccountByEmail => {
+    class LoadAccountByEmailStub implements LoadAccountByEmail {
+        async load(email: string): Promise<AccountModel> {
+            return Promise.resolve(mockAccountModel());
+        }
+    }
+    return new LoadAccountByEmailStub();
+};
---
+++ src/presentation/controllers/account/load-account/load-account-by-email-controller.spec.ts
@@ -0,0 +1,33 @@
+import { mockLoadAccountByEmail } from "@/domain/test";
+import { LoadAccountByEmail } from "@/domain/useCases/account/load-account-by-email";
+import { HttpRequest } from "@/presentation/protocols";
+import { LoadAccountByEmaiLController } from "./load-account-by-email-controller";
+
+const makeFakeRequest = (): HttpRequest => ({
+    body: {
+        email: "any_email@mail.com",
+    },
+});
+
+type SutTypes = {
+    sut: LoadAccountByEmaiLController;
+    loadAccountByEmailStub: LoadAccountByEmail;
+};
+
+const makeSut = (): SutTypes => {
+    const loadAccountByEmailStub = mockLoadAccountByEmail();
+    const sut = new LoadAccountByEmaiLController(loadAccountByEmailStub);
+    return {
+        sut,
+        loadAccountByEmailStub,
+    };
+};
+
+describe("Load Account By Email Controller", () => {
+    test("Should return 200 on LoadAccountByEmail success", async () => {
+        const { sut, loadAccountByEmailStub } = makeSut();
+        const loadSpy = jest.spyOn(loadAccountByEmailStub, "load");
+        await sut.handle(makeFakeRequest());
+        expect(loadSpy).toHaveBeenCalledWith("any_email@mail.com");
+    });
+});
