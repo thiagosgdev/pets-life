48cfd687a007e3d6e39ced61071cf64af7c19ee0
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-03 08:31:17 -0300

    test: ensure LoadAccountByEmailController returns 500 if LoadAccountByEmail throws

--- src/presentation/controllers/account/load-account/load-account-by-email-controller.spec.ts
+++ src/presentation/controllers/account/load-account/load-account-by-email-controller.spec.ts
@@ -46,4 +46,13 @@ describe("Load Account By Email Controller", () => {
         expect(response.status).toBe(200);
         expect(response.body).toBe("Check the e-mail informed");
     });
+
+    test("Should return 200 on LoadAccountByEmail fail", async () => {
+        const { sut, loadAccountByEmailStub } = makeSut();
+        jest.spyOn(loadAccountByEmailStub, "load").mockReturnValueOnce(
+            Promise.reject(new Error()),
+        );
+        const response = await sut.handle(makeFakeRequest());
+        expect(response.status).toBe(500);
+    });
 });
