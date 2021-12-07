259c271e2d7921878233c22251254e5427541e90
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-07 20:18:34 -0300

    feat: ensure LoadPetsByAccountIdController returns 500 if LoadPetsByAccountId throws

--- src/presentation/controllers/pet/load-pet/load-pet-by-account-id-controller.ts
+++ src/presentation/controllers/pet/load-pet/load-pet-by-account-id-controller.ts
@@ -9,19 +9,30 @@ export class LoadPetsByAccountIdController implements Controller {
     constructor(private readonly loadPetsByAccountId: LoadPetsByAccountId) {}
 
     async handle(request: HttpRequest): Promise<HttpResponse> {
-        const { account_id } = request.body;
-        const pets = await this.loadPetsByAccountId.loadByAccountId(account_id);
-        if (pets) {
+        try {
+            const { account_id } = request.body;
+            const pets = await this.loadPetsByAccountId.loadByAccountId(
+                account_id,
+            );
+            if (pets) {
+                return {
+                    status: 200,
+                    body: pets,
+                };
+            }
             return {
                 status: 200,
-                body: pets,
+                body: {
+                    message: "Check the id account",
+                },
+            };
+        } catch (error) {
+            return {
+                status: 500,
+                body: {
+                    message: "Internal Server error",
+                },
             };
         }
-        return {
-            status: 200,
-            body: {
-                message: "Check the id account",
-            },
-        };
     }
 }
