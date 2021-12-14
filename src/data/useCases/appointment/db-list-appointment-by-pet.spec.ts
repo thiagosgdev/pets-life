ed50382c2f24d33174a125b4dc559c7b33ccc1d4
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-14 14:21:56 -0300

    test: ensure DbListAppointmentByPet call ListAppointmentByPet with the correct value

---
+++ src/data/useCases/appointment/db-list-appointment-by-pet.spec.ts
@@ -0,0 +1,39 @@
+import { ListAppointmentByPetRepository } from "@/data/protocols/db/appointment/list-appointment-by-pet-repository";
+import { AppointmentModel } from "@/domain/models/appointment";
+import { mockAppointmentModels } from "@/domain/test";
+import { DbListAppointmentByPet } from "./db-list-appointment-by-pet";
+
+const mockListAppointmentByPetRepository =
+    (): ListAppointmentByPetRepository => {
+        class ListAppointmentByPetRepositoryStub
+            implements ListAppointmentByPetRepository
+        {
+            listByPet(pet_id: string): Promise<AppointmentModel[]> {
+                return Promise.resolve(mockAppointmentModels());
+            }
+        }
+        return new ListAppointmentByPetRepositoryStub();
+    };
+
+type SutTypes = {
+    sut: DbListAppointmentByPet;
+    listAppointmentByPetStub: ListAppointmentByPetRepository;
+};
+
+const makeSut = (): SutTypes => {
+    const listAppointmentByPetStub = mockListAppointmentByPetRepository();
+    const sut = new DbListAppointmentByPet(listAppointmentByPetStub);
+    return {
+        listAppointmentByPetStub,
+        sut,
+    };
+};
+
+describe("Db List Appointment By Pet", () => {
+    test("Should call ListAppointmentByPetRepository with the correct values", async () => {
+        const { sut, listAppointmentByPetStub } = makeSut();
+        const listSpy = jest.spyOn(listAppointmentByPetStub, "listByPet");
+        await sut.listByPet("any_pet_id");
+        expect(listSpy).toHaveBeenCalledWith("any_pet_id");
+    });
+});
--- src/domain/test/mock-appointment.ts
+++ src/domain/test/mock-appointment.ts
@@ -21,6 +21,27 @@ export const mockAddAppointmentParams = (): AddAppointmentParams => ({
     scheduled_date: new Date(),
 });
 
+export const mockAppointmentModels = (): AppointmentModel[] => [
+    {
+        id: "any_id",
+        description: "any_description",
+        pet_id: "any_pet_id",
+        doctor_name: "any_doctor_name",
+        scheduled_date: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
+    },
+    {
+        id: "another_id",
+        description: "another_description",
+        pet_id: "another_pet_id",
+        doctor_name: "another_doctor_name",
+        scheduled_date: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
+    },
+];
+
 export const mockAddAppointment = (): AddAppointment => {
     class AddAppointmentStub implements AddAppointment {
         async add(data: AddAppointmentParams): Promise<AppointmentModel> {
