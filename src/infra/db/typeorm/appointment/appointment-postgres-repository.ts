379b8fc38ae44bfd03b2b70498b29b3b5535b19a
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-15 15:16:52 -0300

    feat: ensure AppointmentPostgresRepository returns the appointments on listByPet() success

--- src/infra/db/typeorm/appointment/appointment-postgres-repository.ts
+++ src/infra/db/typeorm/appointment/appointment-postgres-repository.ts
@@ -4,9 +4,12 @@ import {
     AddAppointment,
     AddAppointmentParams,
 } from "@/domain/useCases/appointment/add-appointment";
+import { ListAppointmentByPet } from "@/domain/useCases/appointment/list-appointment-by-pet";
 import { getRepository, Repository } from "typeorm";
 
-export class AppointmentPostgresRepository implements AddAppointment {
+export class AppointmentPostgresRepository
+    implements AddAppointment, ListAppointmentByPet
+{
     private repository: Repository<Appointment>;
     constructor() {
         this.repository = getRepository(Appointment);
@@ -16,5 +19,12 @@ export class AppointmentPostgresRepository implements AddAppointment {
         const appointment = this.repository.create(data);
         await this.repository.save(appointment);
         return appointment;
+    }
+
+    async listByPet(pet_id: string): Promise<AppointmentModel[]> {
+        const appointments = await this.repository.find({ pet_id });
+        if (appointments.length > 0) {
+            return appointments;
+        }
     }
 }
