ddef39e42ef3fef668bb7f578229f660a8764c03
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-18 15:51:22 -0300

    feat: ensure ListAppointmentByPetController returns 500 if ListAppointmentByPet throws

--- src/presentation/controllers/appointment/list-appointment-by-pet-controller.ts
+++ src/presentation/controllers/appointment/list-appointment-by-pet-controller.ts
@@ -9,19 +9,30 @@ export class ListAppointmentByPetController implements Controller {
     constructor(private readonly listAppointmentByPet: ListAppointmentByPet) {}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const { pet_id } = httpRequest.body;
-        const appointments = await this.listAppointmentByPet.listByPet(pet_id);
-        if (appointments.length > 0) {
+        try {
+            const { pet_id } = httpRequest.body;
+            const appointments = await this.listAppointmentByPet.listByPet(
+                pet_id,
+            );
+            if (appointments.length > 0) {
+                return {
+                    status: 200,
+                    body: appointments,
+                };
+            }
             return {
-                status: 200,
-                body: appointments,
+                status: 204,
+                body: {
+                    message: "Verify the pet id informed",
+                },
+            };
+        } catch (error) {
+            return {
+                status: 500,
+                body: {
+                    message: "Internal Server Error",
+                },
             };
         }
-        return {
-            status: 204,
-            body: {
-                message: "Verify the pet id informed",
-            },
-        };
     }
 }
